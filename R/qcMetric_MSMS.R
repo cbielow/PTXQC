466b74a7a548b376442f8521d03a4c86889c5d7d
Chris Bielow <chris.bielow@fu-berlin.de>
2016-02-08 13:28:49 +0100

    removed UTF-8 char

--- R/qcMetric_MSMS.R
+++ R/qcMetric_MSMS.R
@@ -111,7 +111,7 @@ general, increased MC counts also increase the number of peptide signals, thus c
 space and potentially provoking overlapping peptide signals, biasing peptide quantification.
 Thus, low MC counts should be favored. Interestingly, it has been shown recently that 
 incorporation of peptides with missed cleavages does not negatively influence protein quantification (see 
-[http://pubs.acs.org/doi/abs/10.1021/pr500294d](Chiva, C., Ortega, M., and Sabidó, E. Influence of the Digestion Technique, Protease, and Missed 
+[http://pubs.acs.org/doi/abs/10.1021/pr500294d](Chiva, C., Ortega, M., and Sabido, E. Influence of the Digestion Technique, Protease, and Missed 
 Cleavage Peptides in Protein Quantitation. J. Proteome Res. 2014, 13, 3979-86) ). 
 However this is true only if all samples show the same degree of digestion. High missed cleavage values 
 can indicate for example, either a) failed digestion, b) a high (post-digestion) protein contamination, or 
