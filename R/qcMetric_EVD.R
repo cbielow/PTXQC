1cd96065fd208099736cf855c47a145d79d01559
Chris Bielow <chris.bielow@fu-berlin.de>
2017-10-06 11:46:14 +0200

    [DOC] updated charge documentation

--- R/qcMetric_EVD.R
+++ R/qcMetric_EVD.R
@@ -705,8 +705,11 @@ qcMetric_EVD_Charge =  setRefClass(
   contains = "qcMetric",
   methods = list(initialize=function() {  callSuper(    
     helpTextTemplate = 
-      "Charge distribution per Raw file. Should be dominated by charge 2 
-(one N-terminal and one at tryptic C-terminal R or K residue) and have a similar distribution across Raw files.
+      "Charge distribution per Raw file. For typtic digests, peptides of charge 2 
+(one N-terminal and one at tryptic C-terminal R or K residue) should be dominant.
+Ionization issues (voltage?), in-source fragmentation, missed cleavages and buffer irregularities can 
+cause a shift (see [http://onlinelibrary.wiley.com/doi/10.1002/mas.21544/abstract](Bittremieux 2017, DOI: 10.1002/mas.21544) ).
+The charge distribution should be similar across Raw files.
 Consistent charge distribution is paramount for comparable 3D-peak intensities across samples.
 
 Heatmap score [EVD: Charge]: Deviation of the charge 2 proportion from a representative Raw file ('qualMedianDist' function).
