4356202d41690ad71ba6755811b305378d3a8ac4
Chris Bielow <chris.bielow@fu-berlin.de>
2017-10-06 14:48:23 +0200

    fix param name (augment 0.91)

--- R/fcn_plots.R
+++ R/fcn_plots.R
@@ -513,8 +513,6 @@ plot_MBRgain = function(data, title_sub = "")
 
 
 #'
-#' Plot MaxQuant Match-between-runs id transfer performance.
-#' 
 #' The plots shows the charge distribution per Raw file.
 #' The output of 'mosaicize()' can be used directly.
 #' 
@@ -526,7 +524,7 @@ plot_MBRgain = function(data, title_sub = "")
 #'   'Margin_var1' - 
 #' where each row represents one peptide sequence.
 #' 
-#' @param data A data.frame with columns as described above
+#' @param d_charge A data.frame with columns as described above
 #' @return GGplot object
 #'
 #' @import ggplot2
@@ -534,7 +532,7 @@ plot_MBRgain = function(data, title_sub = "")
 #' 
 #' @examples
 #'  data = data.frame(raw.file = c(rep('file A', 100), rep('file B', 40)),
-#'                      charge = c(rep(2, 60), rep(3, 30), rep(4, 10),
+#'                        data = c(rep(2, 60), rep(3, 30), rep(4, 10),
 #'                                 rep(2, 30), rep(3, 7), rep(4, 3)))
 #'  plot_Charge(mosaicize(data))
 #' 
